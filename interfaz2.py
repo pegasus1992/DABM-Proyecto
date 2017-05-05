a01e936fbde3667d8b1f1cbcb6d00957b250982f
Juan <juan.arevalo.merchan@gmail.com>
2017-05-04 20:09:18 -0500

    Intento

--- interfaz2.py
+++ interfaz2.py
@@ -7,12 +7,6 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QVBoxLayout
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-from random import random
-import matplotlib.pyplot as plt
-from drawnow import *
 
 LIM = 100
 
@@ -33,6 +27,9 @@ class Ui_mainWindow(object):
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
+
+
+
         self.grupoConfiguracion = QtWidgets.QGroupBox(self.centralwidget)
         self.grupoConfiguracion.setGeometry(QtCore.QRect(510, 430, 531, 81))
         font = QtGui.QFont()
@@ -153,10 +150,6 @@ class Ui_mainWindow(object):
         self.retranslateUi(mainWindow)
         QtCore.QMetaObject.connectSlotsByName(mainWindow)
 
-        l = QVBoxLayout(self.main_widget)
-        dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=100)
-        l.addWidget(dc)
-
         self.accionesBotones()
         return
 
@@ -185,44 +178,5 @@ class Ui_mainWindow(object):
         self.pushButton.setText(_translate("mainWindow", "Iniciar Lecturas"))
         self.pushButton_2.setText(_translate("mainWindow", "Detener Lecturas"))
         self.lbl_titulo.setText(_translate("mainWindow", "Monitor de frecuencia cardiaca"))
-
         return
-
-class MyMplCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        # We want the axes cleared every time plot() is called
-        self.axes.hold(False)
-
-        self.compute_initial_figure()
-
-        #
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-
-        #FigureCanvas.setSizePolicy(self,QSizePolicy.Expanding,QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-
-    def compute_initial_figure(self):
-        pass
-
-class MyDynamicMplCanvas(MyMplCanvas):
-    """A canvas that updates itself every second with a new plot."""
-    def __init__(self, *args, **kwargs):
-        MyMplCanvas.__init__(self, *args, **kwargs)
-        timer = QtCore.QTimer(self)
-        timer.timeout.connect(self.update_figure)
-        timer.start(1000)
-
-    def compute_initial_figure(self):
-        self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], 'r')
-
-    def update_figure(self):
-        # Build a list of 4 random integers between 0 and 10 (both inclusive)
-        l = [random.randint(0, 10) for i in range(4)]
-
-        self.axes.plot([0, 1, 2, 3], l, 'r')
-        self.draw()
-
 
